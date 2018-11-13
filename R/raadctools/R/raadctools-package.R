2eaba8d34ab4f95d4bc4d54de20b10c70e4f8cfb
James Eddy <james.a.eddy@gmail.com>
2018-11-13 13:28:57 -0800

    update imports etc. in description/namespace

---
+++ R/raadctools/R/raadctools-package.R
@@ -0,0 +1,12 @@
+#' @import crayon
+#' @import dplyr
+#' @import tidyr
+#' @import testthat
+#' @importFrom glue glue
+#' @importFrom magrittr %>%
+#' @importFrom purrr keep pluck map map2 map_df map_chr map_int map_lgl
+#'   flatten flatten_chr set_names
+#' @importFrom readr write_csv
+#' @importFrom stringr str_c
+#' @importFrom tibble as_tibble
+NULL
