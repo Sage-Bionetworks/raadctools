15cd84572e34a8aab71bcd567c96b926b1d9fc81
Thomas Yu <thomasyu888@gmail.com>
2018-12-27 16:12:52 -0800

    Add in synapse login function

--- python/submitRAADC2/submit.py
+++ python/submitRAADC2/submit.py
@@ -4,7 +4,25 @@ import rpy2.robjects as ro
 import pandas as pd
 r_submitRAADC2 = importr("submitRAADC2")
 from distutils.util import strtobool
+import synapseclient
 
+def synapse_login():
+	syn = synapseclient.Synapse()
+	email = r_submitRAADC2._user_email_prompt()[0]
+	try:
+		syn.login(email=email)
+	except synapseclient.exceptions.SynapseNoCredentialsError as e:
+		syn = _new_login(syn, email)
+	return(syn)
+
+def _new_login(syn, email):
+	print(''.join(r_submitRAADC2._new_login_text()))
+	apikey = r_submitRAADC2._api_key_prompt()[0]
+	try:
+		syn.login(email, apiKey=apikey, rememberMe=True)
+	except Exceptions as e:
+		raise ValueError("Please double check your email and apiKey combination")
+	return(syn)
 
 def submit_raadc2(prediction_filepath, validate_only=False, dry_run=False):
 	'''
@@ -15,8 +33,10 @@ def submit_raadc2(prediction_filepath, validate_only=False, dry_run=False):
 		validate_only: If 'True', check data for any formatting errors but don't submit to the challenge.
 		dry_run: If ‘TRUE', execute submission steps, but don’t store any data in Synapse. 
 	'''
-	username = _user_email_prompt()
-	apikey = _api_key_prompt()
+	# username = r_submitRAADC2._user_email_prompt()
+	# apikey = r_submitRAADC2._api_key_prompt()
+	#username = _user_email_prompt()
+	#apikey = _api_key_prompt()
 
 	team_info = r_submitRAADC2.get_team_info(owner_id)
 	# is_eligible <- check_eligibility(team_info$team_id, owner_id)
@@ -34,13 +54,13 @@ def submit_raadc2(prediction_filepath, validate_only=False, dry_run=False):
 	r_submitRAADC2.submit_raadc2(predictiondf, validate_only=validate_only, dry_run=dry_run)
 
 
-def _user_email_prompt():
-	print(''.join(r_submitRAADC2._user_email_prompt_text()))
-	return input("Username:")
+# def _user_email_prompt():
+# 	print(''.join(r_submitRAADC2._user_email_prompt_text()))
+# 	return input("Username:")
 
-def _api_key_prompt():
-	print(''.join(r_submitRAADC2._api_key_prompt_text()))
-	return input("API key:")
+# def _api_key_prompt():
+# 	print(''.join(r_submitRAADC2._api_key_prompt_text()))
+# 	return input("API key:")
 
 
 def build_parser():
